b6dc297290fc12b040d20544db2af9a02daedec8
Waitsnake <waitsnake@users.noreply.github.com>
2026-03-21 17:17:32 +0100

    move emoji map in extra file do emoji replacement before sanitise

---
+++ emoji_map.py
@@ -0,0 +1,106 @@
+# -------------------------
+# Emoji replacement (spoken)
+# -------------------------
+EMOJI_MAP = {
+
+    # -------------------------
+    # Angel
+    # -------------------------
+    "0:)": {"English":"angel smiley","German":"engel smiley","French":"smiley ange","Spanish":"ángel","Japanese":"エンジェル スマイリー"},
+    "O:)": {"English":"angel smiley","German":"engel smiley","French":"smiley ange","Spanish":"ángel","Japanese":"エンジェル スマイリー"},
+    "O:-)": {"English":"angel smiley","German":"engel smiley","French":"smiley ange","Spanish":"ángel","Japanese":"エンジェル スマイリー"},
+    "0:-)": {"English":"angel smiley","German":"engel smiley","French":"smiley ange","Spanish":"ángel","Japanese":"エンジェル スマイリー"},
+
+    # -------------------------
+    # Happy / Laugh
+    # -------------------------
+    ":)": {"English":"happy smiley","German":"lächelnder smiley","French":"smiley heureux","Spanish":"sonrisa feliz","Japanese":"ハッピー スマイリー"},
+    ":-)": {"English":"happy smiley","German":"lächelnder smiley","French":"smiley heureux","Spanish":"sonrisa feliz","Japanese":"ハッピー スマイリー"},
+    ":D": {"English":"laughing smiley","German":"lachender smiley","French":"smiley qui rit","Spanish":"sonrisa riendo","Japanese":"ラッフィング スマイリー"},
+    ":-D": {"English":"laughing smiley","German":"lachender smiley","French":"smiley qui rit","Spanish":"sonrisa riendo","Japanese":"ラッフィング スマイリー"},
+    "xD": {"English":"laughing smiley","German":"lachender smiley","French":"smiley qui rit","Spanish":"risa","Japanese":"ラッフィング スマイリー"},
+    "XD": {"English":"laughing smiley","German":"lachender smiley","French":"smiley qui rit","Spanish":"risa","Japanese":"ラッフィング スマイリー"},
+
+    # -------------------------
+    # Sad / Cry
+    # -------------------------
+    ":(": {"English":"sad smiley","German":"trauriger smiley","French":"smiley triste","Spanish":"cara triste","Japanese":"サッド スマイリー"},
+    ":c": {"English":"sad smiley","German":"trauriger smiley","French":"smiley triste","Spanish":"cara triste","Japanese":"サッド スマイリー"},
+    ":C": {"English":"sad smiley","German":"trauriger smiley","French":"smiley triste","Spanish":"cara triste","Japanese":"サッド スマイリー"},
+    ":-(": {"English":"sad smiley","German":"trauriger smiley","French":"smiley triste","Spanish":"cara triste","Japanese":"サッド スマイリー"},
+    ":'(": {"English":"crying smiley","German":"weinender smiley","French":"smiley qui pleure","Spanish":"cara llorando","Japanese":"クライング スマイリー"},
+
+    # -------------------------
+    # Wink
+    # -------------------------
+    ";)": {"English":"winking smiley","German":"zwinkernder smiley","French":"smiley clin d'œil","Spanish":"guiño","Japanese":"ウィンク スマイリー"},
+    ";-)": {"English":"winking smiley","German":"zwinkernder smiley","French":"smiley clin d'œil","Spanish":"guiño","Japanese":"ウィンク スマイリー"},
+
+    # -------------------------
+    # Tongue
+    # -------------------------
+    ":P": {"English":"tongue smiley","German":"zunge raus smiley","French":"smiley langue tirée","Spanish":"sacar la lengua","Japanese":"タン スマイリー"},
+    ":-P": {"English":"tongue smiley","German":"zunge raus smiley","French":"smiley langue tirée","Spanish":"sacar la lengua","Japanese":"タン スマイリー"},
+
+    # -------------------------
+    # Surprise
+    # -------------------------
+    ":O": {"English":"surprised smiley","German":"überraschtes smiley","French":"smiley surpris","Spanish":"sorpresa","Japanese":"サプライズ スマイリー"},
+    ":-O": {"English":"surprised smiley","German":"überraschtes smiley","French":"smiley surpris","Spanish":"sorpresa","Japanese":"サプライズ スマイリー"},
+
+    # -------------------------
+    # Confused / unsure
+    # -------------------------
+    ":/": {"English":"unsure smiley","German":"unsicherer smiley","French":"smiley incertain","Spanish":"sonrisa dudosa","Japanese":"アンスア スマイリー"},
+    ":-/": {"English":"unsure smiley","German":"unsicherer smiley","French":"smiley incertain","Spanish":"sonrisa dudosa","Japanese":"アンスア スマイリー"},
+    ":S": {"English":"confused smiley","German":"verwirrter smiley","French":"smiley confus","Spanish":"cara confundida","Japanese":"コンフューズド スマイリー"},
+    ":-S": {"English":"confused smiley","German":"verwirrter smiley","French":"smiley confus","Spanish":"cara confundida","Japanese":"コンフューズド スマイリー"},
+
+    # -------------------------
+    # Kiss / Love
+    # -------------------------
+    ":*": {"English":"kissing smiley","German":"kuss smiley","French":"smiley bisou","Spanish":"beso","Japanese":"キス スマイリー"},
+    ":-*": {"English":"kissing smiley","German":"kuss smiley","French":"smiley bisou","Spanish":"beso","Japanese":"キス スマイリー"},
+
+    # -------------------------
+    # Cool
+    # -------------------------
+    "B)": {"English":"cool smiley","German":"cooler smiley","French":"smiley cool","Spanish":"cara cool","Japanese":"クール スマイリー"},
+    "8)": {"English":"cool smiley","German":"cooler smiley","French":"smiley cool","Spanish":"cara cool","Japanese":"クール スマイリー"},
+
+    # -------------------------
+    # Hearts / Love
+    # -------------------------
+    "<3": {"English":"heart","German":"herz","French":"cœur","Spanish":"corazón","Japanese":"ハート"},
+    "</3": {"English":"broken heart","German":"gebrochenes herz","French":"cœur brisé","Spanish":"corazón roto","Japanese":"ブロークン ハート"},
+    "<33": {"English":"lots of love","German":"viel liebe","French":"beaucoup d'amour","Spanish":"mucho amor","Japanese":"ラブ いっぱい"},
+    "❤": {"English":"heart","German":"herz","French":"cœur","Spanish":"corazón","Japanese":"ハート"},
+    "❤️": {"English":"heart","German":"herz","French":"cœur","Spanish":"corazón","Japanese":"ハート"},
+    "💔": {"English":"broken heart","German":"gebrochenes herz","French":"cœur brisé","Spanish":"corazón roto","Japanese":"ブロークン ハート"},
+
+    # -------------------------
+    # Actions
+    # -------------------------
+    "o/": {"English":"wave","German":"wink","French":"salut","Spanish":"saludo","Japanese":"ウェーブ"},
+    "\\o": {"English":"wave","German":"wink","French":"salut","Spanish":"saludo","Japanese":"ウェーブ"},
+    "\\o/": {"English":"cheer","German":"jubel","French":"acclamation","Spanish":"celebración","Japanese":"チアー"},
+
+    # -------------------------
+    # Cute / Anime / MMO
+    # -------------------------
+    ":3": {"English":"cute smiley","German":"süßer smiley","French":"smiley mignon","Spanish":"cara tierna","Japanese":"キュート スマイリー"},
+    ":33": {"English":"very cute smiley","German":"sehr süßer smiley","French":"smiley très mignon","Spanish":"muy tierno","Japanese":"とてもキュート スマイリー"},
+    "uwu": {"English":"cute face","German":"süßes gesicht","French":"visage mignon","Spanish":"cara tierna","Japanese":"キュート フェイス"},
+    "UwU": {"English":"cute face","German":"süßes gesicht","French":"visage mignon","Spanish":"cara tierna","Japanese":"キュート フェイス"},
+    "owo": {"English":"surprised cute face","German":"überraschtes süßes gesicht","French":"visage mignon surpris","Spanish":"cara tierna sorprendida","Japanese":"サプライズ キュート フェイス"},
+    "OwO": {"English":"surprised cute face","German":"überraschtes süßes gesicht","French":"visage mignon surpris","Spanish":"cara tierna sorprendida","Japanese":"サプライズ キュート フェイス"},
+
+    # -------------------------
+    # ASCII Faces
+    # -------------------------
+    ">_<": {"English":"pain face","German":"aua gesicht","French":"visage douleur","Spanish":"cara de dolor","Japanese":"イタイ フェイス"},
+    ">w<": {"English":"excited cute face","German":"aufgeregtes süßes gesicht","French":"visage mignon excité","Spanish":"cara tierna emocionada","Japanese":"キュート エキサイト フェイス"},
+    "^_^": {"English":"happy face","German":"fröhliches gesicht","French":"visage heureux","Spanish":"cara feliz","Japanese":"ハッピー フェイス"},
+    "^-^": {"English":"happy face","German":"fröhliches gesicht","French":"visage heureux","Spanish":"cara feliz","Japanese":"ハッピー フェイス"},
+    "O_o": {"English":"confused face","German":"verwirrtes gesicht","French":"visage confus","Spanish":"cara confundida","Japanese":"コンフューズド フェイス"},
+}
