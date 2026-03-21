883c8b05ea5e80db823175be4b8ee9e1551385de
Waitsnake <waitsnake@users.noreply.github.com>
2026-03-21 17:02:05 +0100

    add some smiley replacement

--- WinToTalk.py
+++ WinToTalk.py
@@ -158,6 +158,130 @@ def detect_chat_language(text, default_language):
 
     print("[WinToTalk] (default) Detect Language =", default_language)
     return default_language
+    
+# -------------------------
+# Emoji replacement (spoken)
+# -------------------------
+EMOJI_MAP = {
+
+    # -------------------------
+    # Angel
+    # -------------------------
+    "0:)": {"English":"angel smiley","German":"engel smiley","French":"smiley ange","Spanish":"ángel","Japanese":"エンジェル スマイリー"},
+    "O:)": {"English":"angel smiley","German":"engel smiley","French":"smiley ange","Spanish":"ángel","Japanese":"エンジェル スマイリー"},
+    "O:-)": {"English":"angel smiley","German":"engel smiley","French":"smiley ange","Spanish":"ángel","Japanese":"エンジェル スマイリー"},
+    "0:-)": {"English":"angel smiley","German":"engel smiley","French":"smiley ange","Spanish":"ángel","Japanese":"エンジェル スマイリー"},
+
+    # -------------------------
+    # Happy / Laugh
+    # -------------------------
+    ":)": {"English":"happy smiley","German":"lächelnder smiley","French":"smiley heureux","Spanish":"sonrisa feliz","Japanese":"ハッピー スマイリー"},
+    ":-)": {"English":"happy smiley","German":"lächelnder smiley","French":"smiley heureux","Spanish":"sonrisa feliz","Japanese":"ハッピー スマイリー"},
+    ":D": {"English":"laughing smiley","German":"lachender smiley","French":"smiley qui rit","Spanish":"sonrisa riendo","Japanese":"ラッフィング スマイリー"},
+    ":-D": {"English":"laughing smiley","German":"lachender smiley","French":"smiley qui rit","Spanish":"sonrisa riendo","Japanese":"ラッフィング スマイリー"},
+    "xD": {"English":"laughing smiley","German":"lachender smiley","French":"smiley qui rit","Spanish":"risa","Japanese":"ラッフィング スマイリー"},
+    "XD": {"English":"laughing smiley","German":"lachender smiley","French":"smiley qui rit","Spanish":"risa","Japanese":"ラッフィング スマイリー"},
+
+    # -------------------------
+    # Sad / Cry
+    # -------------------------
+    ":(": {"English":"sad smiley","German":"trauriger smiley","French":"smiley triste","Spanish":"cara triste","Japanese":"サッド スマイリー"},
+    ":-(": {"English":"sad smiley","German":"trauriger smiley","French":"smiley triste","Spanish":"cara triste","Japanese":"サッド スマイリー"},
+    ":'(": {"English":"crying smiley","German":"weinender smiley","French":"smiley qui pleure","Spanish":"cara llorando","Japanese":"クライング スマイリー"},
+
+    # -------------------------
+    # Wink
+    # -------------------------
+    ";)": {"English":"winking smiley","German":"zwinkernder smiley","French":"smiley clin d'œil","Spanish":"guiño","Japanese":"ウィンク スマイリー"},
+    ";-)": {"English":"winking smiley","German":"zwinkernder smiley","French":"smiley clin d'œil","Spanish":"guiño","Japanese":"ウィンク スマイリー"},
+
+    # -------------------------
+    # Tongue
+    # -------------------------
+    ":P": {"English":"tongue smiley","German":"zunge raus smiley","French":"smiley langue tirée","Spanish":"sacar la lengua","Japanese":"タン スマイリー"},
+    ":-P": {"English":"tongue smiley","German":"zunge raus smiley","French":"smiley langue tirée","Spanish":"sacar la lengua","Japanese":"タン スマイリー"},
+
+    # -------------------------
+    # Surprise
+    # -------------------------
+    ":O": {"English":"surprised smiley","German":"überraschtes smiley","French":"smiley surpris","Spanish":"sorpresa","Japanese":"サプライズ スマイリー"},
+    ":-O": {"English":"surprised smiley","German":"überraschtes smiley","French":"smiley surpris","Spanish":"sorpresa","Japanese":"サプライズ スマイリー"},
+
+    # -------------------------
+    # Confused / unsure
+    # -------------------------
+    ":/": {"English":"unsure smiley","German":"unsicherer smiley","French":"smiley incertain","Spanish":"sonrisa dudosa","Japanese":"アンスア スマイリー"},
+    ":-/": {"English":"unsure smiley","German":"unsicherer smiley","French":"smiley incertain","Spanish":"sonrisa dudosa","Japanese":"アンスア スマイリー"},
+    ":S": {"English":"confused smiley","German":"verwirrter smiley","French":"smiley confus","Spanish":"cara confundida","Japanese":"コンフューズド スマイリー"},
+    ":-S": {"English":"confused smiley","German":"verwirrter smiley","French":"smiley confus","Spanish":"cara confundida","Japanese":"コンフューズド スマイリー"},
+
+    # -------------------------
+    # Kiss / Love
+    # -------------------------
+    ":*": {"English":"kissing smiley","German":"kuss smiley","French":"smiley bisou","Spanish":"beso","Japanese":"キス スマイリー"},
+    ":-*": {"English":"kissing smiley","German":"kuss smiley","French":"smiley bisou","Spanish":"beso","Japanese":"キス スマイリー"},
+
+    # -------------------------
+    # Cool
+    # -------------------------
+    "B)": {"English":"cool smiley","German":"cooler smiley","French":"smiley cool","Spanish":"cara cool","Japanese":"クール スマイリー"},
+    "8)": {"English":"cool smiley","German":"cooler smiley","French":"smiley cool","Spanish":"cara cool","Japanese":"クール スマイリー"},
+
+    # -------------------------
+    # Hearts / Love
+    # -------------------------
+    "<3": {"English":"heart","German":"herz","French":"cœur","Spanish":"corazón","Japanese":"ハート"},
+    "</3": {"English":"broken heart","German":"gebrochenes herz","French":"cœur brisé","Spanish":"corazón roto","Japanese":"ブロークン ハート"},
+    "<33": {"English":"lots of love","German":"viel liebe","French":"beaucoup d'amour","Spanish":"mucho amor","Japanese":"ラブ いっぱい"},
+    "❤": {"English":"heart","German":"herz","French":"cœur","Spanish":"corazón","Japanese":"ハート"},
+    "❤️": {"English":"heart","German":"herz","French":"cœur","Spanish":"corazón","Japanese":"ハート"},
+    "💔": {"English":"broken heart","German":"gebrochenes herz","French":"cœur brisé","Spanish":"corazón roto","Japanese":"ブロークン ハート"},
+
+    # -------------------------
+    # Actions
+    # -------------------------
+    "o/": {"English":"wave","German":"wink","French":"salut","Spanish":"saludo","Japanese":"ウェーブ"},
+    "\\o": {"English":"wave","German":"wink","French":"salut","Spanish":"saludo","Japanese":"ウェーブ"},
+    "\\o/": {"English":"cheer","German":"jubel","French":"acclamation","Spanish":"celebración","Japanese":"チアー"},
+
+    # -------------------------
+    # Cute / Anime / MMO
+    # -------------------------
+    ":3": {"English":"cute smiley","German":"süßer smiley","French":"smiley mignon","Spanish":"cara tierna","Japanese":"キュート スマイリー"},
+    ":33": {"English":"very cute smiley","German":"sehr süßer smiley","French":"smiley très mignon","Spanish":"muy tierno","Japanese":"とてもキュート スマイリー"},
+    "uwu": {"English":"cute face","German":"süßes gesicht","French":"visage mignon","Spanish":"cara tierna","Japanese":"キュート フェイス"},
+    "UwU": {"English":"cute face","German":"süßes gesicht","French":"visage mignon","Spanish":"cara tierna","Japanese":"キュート フェイス"},
+    "owo": {"English":"surprised cute face","German":"überraschtes süßes gesicht","French":"visage mignon surpris","Spanish":"cara tierna sorprendida","Japanese":"サプライズ キュート フェイス"},
+    "OwO": {"English":"surprised cute face","German":"überraschtes süßes gesicht","French":"visage mignon surpris","Spanish":"cara tierna sorprendida","Japanese":"サプライズ キュート フェイス"},
+
+    # -------------------------
+    # ASCII Faces
+    # -------------------------
+    ">_<": {"English":"pain face","German":"aua gesicht","French":"visage douleur","Spanish":"cara de dolor","Japanese":"イタイ フェイス"},
+    ">w<": {"English":"excited cute face","German":"aufgeregtes süßes gesicht","French":"visage mignon excité","Spanish":"cara tierna emocionada","Japanese":"キュート エキサイト フェイス"},
+    "^_^": {"English":"happy face","German":"fröhliches gesicht","French":"visage heureux","Spanish":"cara feliz","Japanese":"ハッピー フェイス"},
+    "^-^": {"English":"happy face","German":"fröhliches gesicht","French":"visage heureux","Spanish":"cara feliz","Japanese":"ハッピー フェイス"},
+    "O_o": {"English":"confused face","German":"verwirrtes gesicht","French":"visage confus","Spanish":"cara confundida","Japanese":"コンフューズド フェイス"},
+}
+
+def replace_emojis_for_speech(text, language, default_language="English"):
+    for emo, translations in EMOJI_MAP.items():
+        if emo in text:
+            replacement = translations.get(language)
+
+            if replacement is None:
+                replacement = translations.get(default_language)
+                
+            if replacement is None:
+                replacement = translations.get("English")
+
+            if replacement is None:
+                continue  # nichts ersetzen
+
+            print("replacement: "+replacement)
+            text = text.replace(emo, replacement)
+
+    return text    
 
 @dataclass
 class SpeechItem:
@@ -343,7 +467,11 @@ def tts_worker():
 
             try:
                 safe_text = sanitize_for_sapi(item.text)
-                voice.Speak(safe_text, SVS_ASYNC)
+                
+                # Emojis nach Detection in text ersetzen
+                emoji_text = replace_emojis_for_speech(safe_text, language, item.language)
+
+                voice.Speak(emoji_text, SVS_ASYNC)
             except Exception as e:
                 print("[TTS] Recovering from SAPI error:", e)
                 voice = comtypes.client.CreateObject("SAPI.SpVoice")
